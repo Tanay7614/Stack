99588e84d7dc529d43e9f2f7f5d9c190456db0a7
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-23 14:12:14 +0530

    Push Element in Stack

---
+++ Mylink.java
@@ -0,0 +1,18 @@
+package sample2;
+
+public class Mylink {
+	
+	 public void makeNode() {
+		  
+		   Node first=new Node(30);
+		   Node second=new Node(56);
+		   Node third=new Node(70);
+		   Stack list=new Stack();
+		   list.push(first);
+		   list.push(third);
+		   list.push(second);
+		   
+		   
+	  }
+
+}
---
+++ Stack.java
@@ -0,0 +1,9 @@
+package sample2;
+
+public class Stack {
+      public void push(Node newNode) {
+    	  Mylinkedlist link=new  Mylinkedlist();
+    	  link.append(newNode);
+    	  link.print();
+      }
+}
