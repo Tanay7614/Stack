df6c464e70d467957e4d25034425c3d06db33035
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 11:32:10 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -3,9 +3,9 @@ package sample2;
 public class Queue {
 	public final Mylinkedlist link = new Mylinkedlist();
     public void enqueue(Node newNode) 
-    {
-    link.append(newNode);
-     }
+      {
+          link.append(newNode);
+      }
 
 	public void dequeue() 
 	{
