a52c9fb9e67d30a87abf224f57b982c1b3bb7da9
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 11:58:18 +0530

    Stack Operation

---
+++ Stack.java
@@ -0,0 +1,26 @@
+package sample2;
+
+public class Stack {
+	public final Mylinkedlist link = new Mylinkedlist();
+    public void push(Node newNode)
+    {
+    link.add(newNode);
+    }
+
+	public void pop() 
+	{
+		 link.pop();
+	}
+
+	public void print()
+	{
+		 link.print();
+		
+	}
+
+	public int  peak() 
+	{
+		int value=link.peak();
+		return value;
+	}
+ }
