6eda8bffe76f85c4040363b7698be3bff32d05d1
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 11:59:23 +0530

    Stack operation

--- Stack.java
+++ Stack.java
@@ -2,10 +2,10 @@ package sample2;
 
 public class Stack {
 	public final Mylinkedlist link = new Mylinkedlist();
-    public void push(Node newNode)
-    {
-    link.add(newNode);
-    }
+        public void push(Node newNode)
+        {
+             link.add(newNode);
+        }
 
 	public void pop() 
 	{
